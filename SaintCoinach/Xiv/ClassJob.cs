0087453d54093cce13d479e49db00b0a06878fdd
Rawaho <rawahodev@gmail.com>
2017-08-25 19:56:23 +1200

    Named ClassId column in ClassJob sheet.

--- SaintCoinach/Xiv/ClassJob.cs
+++ SaintCoinach/Xiv/ClassJob.cs
@@ -16,6 +16,7 @@ namespace SaintCoinach.Xiv {
         public Text.XivString Name { get { return AsString("Name"); } }
         public Text.XivString Abbreviation { get { return AsString("Abbreviation"); } }
         public ClassJobCategory ClassJobCategory { get { return As<ClassJobCategory>(); } }
+        public sbyte ClassId { get { return As<sbyte>("ClassId"); } }
         public ClassJob ParentClassJob { get { return As<ClassJob>("ClassJob{Parent}"); } }
         public Item StartingWeapon { get { return As<Item>("Item{StartingWeapon}"); } }
         public Item SoulCrystal { get { return As<Item>("Item{SoulCrystal}"); } }
